0f3ea0307f2db812fd7e666904fd3d86e51f970d
naser1945 <naser1945.2@gmail.com>
2024-02-06 08:38:25 +0300

    init

---
+++ README.md
@@ -0,0 +1,34 @@
+# Value App
+
+
+## Pre-requisites
+- [Node.js](https://nodejs.org/en/)
+- [Docker](https://docs.docker.com/engine/install/)
+
+## Installation
+1. Clone the repository
+2. Install the dependencies
+```bash
+npm install
+```
+3. start the database using [docker](https://docs.docker.com/engine/install/)
+```bash
+docker run --name value-app-postgres -e POSTGRES_PASSWORD=password -e POSTGRES_DB=value-app -d -p 5432:5432 docker.io/postgres
+```
+4. create .env file in the root of the project and add the following
+```bash
+DATABASE_URL="postgresql://postgres:password@localhost:5432/value-app"
+GOOGLE_CLIENT_ID="354269438319-0cbp494k0k8bpni739m81lbh05sv1od3.apps.googleusercontent.com"
+GOOGLE_CLIENT_SECRET="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"
+NEXTAUTH_SECRET="Pa17nNbqBHmxHUujDxFe5c1wGLTEA2isjDSGFOjz0T8="
+NEXTAUTH_URL="http://localhost:3000"
+```
+5. run prisma push to create the database schema
+```bash
+npm run  db:push
+```
+6. run the application
+```bash
+npm run dev
+```
+7. open the application in the browser at [http://localhost:3000](http://localhost:3000)
---
+++ src/pages/index.tsx
@@ -0,0 +1,47 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+
+import { api } from "~/utils/api";
+
+export default function Home() {
+  const hello = api.post.hello.useQuery({ text: "from tRPC" });
+
+  return (
+    <main className=" flex min-h-screen flex-col items-center justify-center">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+          Value App
+        </h1>
+        <div className="flex flex-col items-center gap-2">
+          <p className="text-2xl">
+            {hello.data ? hello.data.greeting : "Loading tRPC query..."}
+          </p>
+          <AuthShowcase />
+        </div>
+      </div>
+    </main>
+  );
+}
+
+function AuthShowcase() {
+  const { data: sessionData } = useSession();
+
+  const { data: secretMessage } = api.post.getSecretMessage.useQuery(
+    undefined, // no input
+    { enabled: sessionData?.user !== undefined },
+  );
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-4">
+      <p className="text-center text-2xl ">
+        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+        {secretMessage && <span> - {secretMessage}</span>}
+      </p>
+      <button
+        className="rounded-full bg-gray-300 px-10 py-3 font-semibold no-underline transition hover:bg-gray-400"
+        onClick={sessionData ? () => void signOut() : () => void signIn()}
+      >
+        {sessionData ? "Sign out" : "Sign in"}
+      </button>
+    </div>
+  );
+}
