7f63ae4950b4b61623f9dc34eaa4360cc8d4c6ef
naser1945 <naser1945.2@gmail.com>
2024-03-21 04:52:23 +0300

    pwa & icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,9 @@ export default function Document() {
           href="https://api.mapbox.com/mapbox-gl-js/v2.8.1/mapbox-gl.css"
           rel="stylesheet"
         />
+        <meta name="description" content="Value App" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <body>
         <Main />
