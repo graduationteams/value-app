5e57dbd474494eb223e7eb7431a45d65a252d269
naser1945 <naser1945.2@gmail.com>
2024-05-05 18:01:23 +0300

    fix: :bug: bottomSheet shown under the header
    close #6

--- src/components/Bottomsheet/bottomsheet.tsx
+++ src/components/Bottomsheet/bottomsheet.tsx
@@ -23,7 +23,7 @@ export function MyDrawer({
     >
       <Drawer.Portal>
         <Drawer.Overlay className="fixed inset-0 bg-black-B200/40" />
-        <Drawer.Content className="fixed bottom-0 left-0 right-0 flex flex-col rounded-t-[10px] bg-white-W75">
+        <Drawer.Content className="fixed bottom-0 left-0 right-0 z-50 flex flex-col rounded-t-[10px] bg-white-W75">
           <div className="mx-auto mb-8 mt-4 h-1.5 w-24 flex-shrink-0 rounded-full bg-zinc-300" />
 
           <div>{children}</div>
